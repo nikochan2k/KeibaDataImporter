d56e6ad64665fed307fa98d85e42eaf5ee014977
Yoshihiro Seki <nikochan2k@gmail.com>
2017-11-04 18:30:25 +0900

    KolUmaToolのリファクタリング

--- src/app/reader/KOL/KD3/KolKet5Kd3.ts
+++ src/app/reader/KOL/KD3/KolKet5Kd3.ts
@@ -5,7 +5,7 @@ import * as $U from "../../../converters/Uma";
 import { UmaDao } from "../../../daos/UmaDao";
 import { Uma } from "../../../entities/Uma";
 import { DataToImport } from "../../DataToImport";
-import { KolUmaTool } from "../KolUmaTool";
+import { Tool } from "../../Tool";
 import {
   readInt,
   readStr,
@@ -14,17 +14,60 @@ import {
 @Service()
 export class KolKet5Kd3 extends DataToImport {
 
+  @Inject()
+  private tool: Tool;
+
   @OrmManager()
   protected entityManager: EntityManager;
 
   @Inject()
   private umaDao: UmaDao;
 
-  @Inject()
-  private kolUmaTool: KolUmaTool;
-
   protected getBufferLength() {
     return 4002;
+  }
+
+  protected async saveSosenba123(buffer: Buffer, offset: number, seibetsu: $U.Seibetsu, chichiUmaId?: number, hahaUmaId?: number) {
+    const kanaBamei = readStr(buffer, offset + 7, 40);
+    const eigoBamei = readStr(buffer, offset + 47, 40);
+    if (!kanaBamei && !eigoBamei) {
+      return null;
+    }
+
+    const uma = new Uma();
+    uma.Seibetsu = seibetsu;
+    uma.KolUmaCode = readInt(buffer, offset, 7);
+    uma.KanaBamei = kanaBamei;
+    uma.EigoBamei = eigoBamei;
+    uma.Seinen = readInt(buffer, offset + 87, 4);
+    uma.Keiro = $U.keiro.toCodeFromKol(buffer, offset + 91, 2);
+    uma.Kesshu = $U.keiro.toCodeFromKol(buffer, offset + 93, 2);
+    uma.SanchiCode = readStr(buffer, offset + 95, 3);
+    uma.ChichiUmaId = chichiUmaId;
+    uma.HahaUmaId = hahaUmaId;
+
+    return this.umaDao.saveUma(uma, true);
+  }
+
+  protected async saveSosenba45(buffer: Buffer, offset: number, seibetsu: $U.Seibetsu, chichiUmaId?: number, hahaUmaId?: number) {
+    const bamei = readStr(buffer, offset + 7, 40);
+    if (!bamei) {
+      return null;
+    }
+
+    const uma = new Uma();
+    uma.Seibetsu = seibetsu;
+    uma.KolUmaCode = readInt(buffer, offset, 7);
+    if (this.tool.isEnglish(bamei)) {
+      uma.EigoBamei = bamei;
+    } else {
+      uma.KanaBamei = bamei;
+    }
+    uma.SanchiCode = readStr(buffer, offset + 47, 3);
+    uma.ChichiUmaId = chichiUmaId;
+    uma.HahaUmaId = hahaUmaId;
+
+    return this.umaDao.saveUma(uma, true);
   }
 
   public async save(buffer: Buffer) {
@@ -44,120 +87,120 @@ export class KolKet5Kd3 extends DataToImport {
   public async saveH(buffer: Buffer) {
     const hhh = await this.saveHhh(buffer);
     const hhc = await this.saveHhc(buffer);
-    const hh = await this.kolUmaTool.saveSosenba123(buffer, 540, $U.Seibetsu.Hinba, hhc.Id, hhh.Id);
+    const hh = await this.saveSosenba123(buffer, 540, $U.Seibetsu.Hinba, hhc.Id, hhh.Id);
 
     const hch = await this.saveHch(buffer);
     const hcc = await this.saveHcc(buffer);
-    const hc = await this.kolUmaTool.saveSosenba123(buffer, 442, $U.Seibetsu.Hinba, hcc.Id, hch.Id);
+    const hc = await this.saveSosenba123(buffer, 442, $U.Seibetsu.Hinba, hcc.Id, hch.Id);
 
-    return await this.kolUmaTool.saveSosenba123(buffer, 148, $U.Seibetsu.Hinba, hc.Id, hh.Id);
+    return await this.saveSosenba123(buffer, 148, $U.Seibetsu.Hinba, hc.Id, hh.Id);
   }
 
   public async saveC(buffer: Buffer) {
     const chh = await this.saveChh(buffer);
     const chc = await this.saveChc(buffer);
-    const ch = await this.kolUmaTool.saveSosenba123(buffer, 344, $U.Seibetsu.Hinba, chc.Id, chh.Id);
+    const ch = await this.saveSosenba123(buffer, 344, $U.Seibetsu.Hinba, chc.Id, chh.Id);
 
     const cch = await this.saveCch(buffer);
     const ccc = await this.saveCcc(buffer);
-    const cc = await this.kolUmaTool.saveSosenba123(buffer, 246, $U.Seibetsu.Hinba, ccc.Id, cch.Id);
+    const cc = await this.saveSosenba123(buffer, 246, $U.Seibetsu.Hinba, ccc.Id, cch.Id);
 
-    return await this.kolUmaTool.saveSosenba123(buffer, 50, $U.Seibetsu.Hinba, cc.Id, ch.Id);
+    return await this.saveSosenba123(buffer, 50, $U.Seibetsu.Hinba, cc.Id, ch.Id);
   }
 
   public async saveHhh(buffer: Buffer) {
-    const hhhhh = await this.kolUmaTool.saveSosenba45(buffer, 3772, $U.Seibetsu.Hinba);
-    const hhhhc = await this.kolUmaTool.saveSosenba45(buffer, 3722, $U.Seibetsu.Boba);
-    const hhhh = await this.kolUmaTool.saveSosenba45(buffer, 2172, $U.Seibetsu.Hinba, hhhhc.Id, hhhhh.Id);
+    const hhhhh = await this.saveSosenba45(buffer, 3772, $U.Seibetsu.Hinba);
+    const hhhhc = await this.saveSosenba45(buffer, 3722, $U.Seibetsu.Boba);
+    const hhhh = await this.saveSosenba45(buffer, 2172, $U.Seibetsu.Hinba, hhhhc.Id, hhhhh.Id);
 
-    const hhhch = await this.kolUmaTool.saveSosenba45(buffer, 3672, $U.Seibetsu.Hinba);
-    const hhhcc = await this.kolUmaTool.saveSosenba45(buffer, 3622, $U.Seibetsu.Boba);
-    const hhhc = await this.kolUmaTool.saveSosenba45(buffer, 2122, $U.Seibetsu.Boba, hhhcc.Id, hhhch.Id);
+    const hhhch = await this.saveSosenba45(buffer, 3672, $U.Seibetsu.Hinba);
+    const hhhcc = await this.saveSosenba45(buffer, 3622, $U.Seibetsu.Boba);
+    const hhhc = await this.saveSosenba45(buffer, 2122, $U.Seibetsu.Boba, hhhcc.Id, hhhch.Id);
 
-    return this.kolUmaTool.saveSosenba123(buffer, 1324, $U.Seibetsu.Hinba, hhhc.Id, hhhh.Id);
+    return this.saveSosenba123(buffer, 1324, $U.Seibetsu.Hinba, hhhc.Id, hhhh.Id);
   }
 
   public async saveHhc(buffer: Buffer) {
-    const hhchh = await this.kolUmaTool.saveSosenba45(buffer, 3572, $U.Seibetsu.Hinba);
-    const hhchc = await this.kolUmaTool.saveSosenba45(buffer, 3522, $U.Seibetsu.Boba);
-    const hhch = await this.kolUmaTool.saveSosenba45(buffer, 2072, $U.Seibetsu.Hinba, hhchc.Id, hhchh.Id);
+    const hhchh = await this.saveSosenba45(buffer, 3572, $U.Seibetsu.Hinba);
+    const hhchc = await this.saveSosenba45(buffer, 3522, $U.Seibetsu.Boba);
+    const hhch = await this.saveSosenba45(buffer, 2072, $U.Seibetsu.Hinba, hhchc.Id, hhchh.Id);
 
-    const hhcch = await this.kolUmaTool.saveSosenba45(buffer, 3472, $U.Seibetsu.Hinba);
-    const hhccc = await this.kolUmaTool.saveSosenba45(buffer, 3422, $U.Seibetsu.Boba);
-    const hhcc = await this.kolUmaTool.saveSosenba45(buffer, 2022, $U.Seibetsu.Boba, hhccc.Id, hhcch.Id);
+    const hhcch = await this.saveSosenba45(buffer, 3472, $U.Seibetsu.Hinba);
+    const hhccc = await this.saveSosenba45(buffer, 3422, $U.Seibetsu.Boba);
+    const hhcc = await this.saveSosenba45(buffer, 2022, $U.Seibetsu.Boba, hhccc.Id, hhcch.Id);
 
-    return this.kolUmaTool.saveSosenba123(buffer, 1226, $U.Seibetsu.Hinba, hhcc.Id, hhch.Id);
+    return this.saveSosenba123(buffer, 1226, $U.Seibetsu.Hinba, hhcc.Id, hhch.Id);
   }
 
   public async saveHch(buffer: Buffer) {
-    const hchhh = await this.kolUmaTool.saveSosenba45(buffer, 3372, $U.Seibetsu.Hinba);
-    const hchhc = await this.kolUmaTool.saveSosenba45(buffer, 3322, $U.Seibetsu.Boba);
-    const hchh = await this.kolUmaTool.saveSosenba45(buffer, 1972, $U.Seibetsu.Hinba, hchhc.Id, hchhh.Id);
+    const hchhh = await this.saveSosenba45(buffer, 3372, $U.Seibetsu.Hinba);
+    const hchhc = await this.saveSosenba45(buffer, 3322, $U.Seibetsu.Boba);
+    const hchh = await this.saveSosenba45(buffer, 1972, $U.Seibetsu.Hinba, hchhc.Id, hchhh.Id);
 
-    const hchch = await this.kolUmaTool.saveSosenba45(buffer, 3272, $U.Seibetsu.Hinba);
-    const hchcc = await this.kolUmaTool.saveSosenba45(buffer, 3222, $U.Seibetsu.Boba);
-    const hchc = await this.kolUmaTool.saveSosenba45(buffer, 1922, $U.Seibetsu.Boba, hchcc.Id, hchch.Id);
+    const hchch = await this.saveSosenba45(buffer, 3272, $U.Seibetsu.Hinba);
+    const hchcc = await this.saveSosenba45(buffer, 3222, $U.Seibetsu.Boba);
+    const hchc = await this.saveSosenba45(buffer, 1922, $U.Seibetsu.Boba, hchcc.Id, hchch.Id);
 
-    return this.kolUmaTool.saveSosenba123(buffer, 1128, $U.Seibetsu.Hinba, hchh.Id, hchc.Id);
+    return this.saveSosenba123(buffer, 1128, $U.Seibetsu.Hinba, hchh.Id, hchc.Id);
   }
 
   public async saveHcc(buffer: Buffer) {
-    const hcchh = await this.kolUmaTool.saveSosenba45(buffer, 3172, $U.Seibetsu.Hinba);
-    const hcchc = await this.kolUmaTool.saveSosenba45(buffer, 3122, $U.Seibetsu.Boba);
-    const hcch = await this.kolUmaTool.saveSosenba45(buffer, 1872, $U.Seibetsu.Hinba, hcchc.Id, hcchh.Id);
+    const hcchh = await this.saveSosenba45(buffer, 3172, $U.Seibetsu.Hinba);
+    const hcchc = await this.saveSosenba45(buffer, 3122, $U.Seibetsu.Boba);
+    const hcch = await this.saveSosenba45(buffer, 1872, $U.Seibetsu.Hinba, hcchc.Id, hcchh.Id);
 
-    const hccch = await this.kolUmaTool.saveSosenba45(buffer, 3072, $U.Seibetsu.Hinba);
-    const hcccc = await this.kolUmaTool.saveSosenba45(buffer, 3022, $U.Seibetsu.Boba);
-    const hccc = await this.kolUmaTool.saveSosenba45(buffer, 1822, $U.Seibetsu.Boba, hcccc.Id, hccch.Id);
+    const hccch = await this.saveSosenba45(buffer, 3072, $U.Seibetsu.Hinba);
+    const hcccc = await this.saveSosenba45(buffer, 3022, $U.Seibetsu.Boba);
+    const hccc = await this.saveSosenba45(buffer, 1822, $U.Seibetsu.Boba, hcccc.Id, hccch.Id);
 
-    return this.kolUmaTool.saveSosenba123(buffer, 1030, $U.Seibetsu.Hinba, hccc.Id, hcch.Id);
+    return this.saveSosenba123(buffer, 1030, $U.Seibetsu.Hinba, hccc.Id, hcch.Id);
   }
 
   public async saveChh(buffer: Buffer) {
-    const chhhh = await this.kolUmaTool.saveSosenba45(buffer, 2972, $U.Seibetsu.Hinba);
-    const chhhc = await this.kolUmaTool.saveSosenba45(buffer, 2922, $U.Seibetsu.Boba);
-    const chhh = await this.kolUmaTool.saveSosenba45(buffer, 1772, $U.Seibetsu.Hinba, chhhc.Id, chhhh.Id);
+    const chhhh = await this.saveSosenba45(buffer, 2972, $U.Seibetsu.Hinba);
+    const chhhc = await this.saveSosenba45(buffer, 2922, $U.Seibetsu.Boba);
+    const chhh = await this.saveSosenba45(buffer, 1772, $U.Seibetsu.Hinba, chhhc.Id, chhhh.Id);
 
-    const chhch = await this.kolUmaTool.saveSosenba45(buffer, 2872, $U.Seibetsu.Hinba);
-    const chhcc = await this.kolUmaTool.saveSosenba45(buffer, 2822, $U.Seibetsu.Boba);
-    const chhc = await this.kolUmaTool.saveSosenba45(buffer, 1722, $U.Seibetsu.Boba, chhcc.Id, chhch.Id);
+    const chhch = await this.saveSosenba45(buffer, 2872, $U.Seibetsu.Hinba);
+    const chhcc = await this.saveSosenba45(buffer, 2822, $U.Seibetsu.Boba);
+    const chhc = await this.saveSosenba45(buffer, 1722, $U.Seibetsu.Boba, chhcc.Id, chhch.Id);
 
-    return this.kolUmaTool.saveSosenba123(buffer, 932, $U.Seibetsu.Hinba, chhc.Id, chhh.Id);
+    return this.saveSosenba123(buffer, 932, $U.Seibetsu.Hinba, chhc.Id, chhh.Id);
   }
 
   public async saveChc(buffer: Buffer) {
-    const chchh = await this.kolUmaTool.saveSosenba45(buffer, 2772, $U.Seibetsu.Hinba);
-    const chchc = await this.kolUmaTool.saveSosenba45(buffer, 2722, $U.Seibetsu.Boba);
-    const chch = await this.kolUmaTool.saveSosenba45(buffer, 1672, $U.Seibetsu.Hinba, chchc.Id, chchh.Id);
+    const chchh = await this.saveSosenba45(buffer, 2772, $U.Seibetsu.Hinba);
+    const chchc = await this.saveSosenba45(buffer, 2722, $U.Seibetsu.Boba);
+    const chch = await this.saveSosenba45(buffer, 1672, $U.Seibetsu.Hinba, chchc.Id, chchh.Id);
 
-    const chcch = await this.kolUmaTool.saveSosenba45(buffer, 2672, $U.Seibetsu.Hinba);
-    const chccc = await this.kolUmaTool.saveSosenba45(buffer, 2622, $U.Seibetsu.Boba);
-    const chcc = await this.kolUmaTool.saveSosenba45(buffer, 1622, $U.Seibetsu.Boba, chccc.Id, chcch.Id);
+    const chcch = await this.saveSosenba45(buffer, 2672, $U.Seibetsu.Hinba);
+    const chccc = await this.saveSosenba45(buffer, 2622, $U.Seibetsu.Boba);
+    const chcc = await this.saveSosenba45(buffer, 1622, $U.Seibetsu.Boba, chccc.Id, chcch.Id);
 
-    return this.kolUmaTool.saveSosenba123(buffer, 834, $U.Seibetsu.Hinba, chcc.Id, chch.Id);
+    return this.saveSosenba123(buffer, 834, $U.Seibetsu.Hinba, chcc.Id, chch.Id);
   }
 
   public async saveCch(buffer: Buffer) {
-    const cchhh = await this.kolUmaTool.saveSosenba45(buffer, 2572, $U.Seibetsu.Hinba);
-    const cchhc = await this.kolUmaTool.saveSosenba45(buffer, 2522, $U.Seibetsu.Boba);
-    const cchh = await this.kolUmaTool.saveSosenba45(buffer, 1572, $U.Seibetsu.Hinba, cchhc.Id, cchhh.Id);
+    const cchhh = await this.saveSosenba45(buffer, 2572, $U.Seibetsu.Hinba);
+    const cchhc = await this.saveSosenba45(buffer, 2522, $U.Seibetsu.Boba);
+    const cchh = await this.saveSosenba45(buffer, 1572, $U.Seibetsu.Hinba, cchhc.Id, cchhh.Id);
 
-    const cchch = await this.kolUmaTool.saveSosenba45(buffer, 2472, $U.Seibetsu.Hinba);
-    const cchcc = await this.kolUmaTool.saveSosenba45(buffer, 2422, $U.Seibetsu.Boba);
-    const cchc = await this.kolUmaTool.saveSosenba45(buffer, 1522, $U.Seibetsu.Boba, cchcc.Id, cchch.Id);
+    const cchch = await this.saveSosenba45(buffer, 2472, $U.Seibetsu.Hinba);
+    const cchcc = await this.saveSosenba45(buffer, 2422, $U.Seibetsu.Boba);
+    const cchc = await this.saveSosenba45(buffer, 1522, $U.Seibetsu.Boba, cchcc.Id, cchch.Id);
 
-    return this.kolUmaTool.saveSosenba123(buffer, 736, $U.Seibetsu.Hinba, cchc.Id, cchh.Id);
+    return this.saveSosenba123(buffer, 736, $U.Seibetsu.Hinba, cchc.Id, cchh.Id);
   }
 
   public async saveCcc(buffer: Buffer) {
-    const ccchh = await this.kolUmaTool.saveSosenba45(buffer, 2372, $U.Seibetsu.Hinba);
-    const ccchc = await this.kolUmaTool.saveSosenba45(buffer, 2322, $U.Seibetsu.Boba);
-    const ccch = await this.kolUmaTool.saveSosenba45(buffer, 1472, $U.Seibetsu.Hinba, ccchc.Id, ccchh.Id);
+    const ccchh = await this.saveSosenba45(buffer, 2372, $U.Seibetsu.Hinba);
+    const ccchc = await this.saveSosenba45(buffer, 2322, $U.Seibetsu.Boba);
+    const ccch = await this.saveSosenba45(buffer, 1472, $U.Seibetsu.Hinba, ccchc.Id, ccchh.Id);
 
-    const cccch = await this.kolUmaTool.saveSosenba45(buffer, 2272, $U.Seibetsu.Hinba);
-    const ccccc = await this.kolUmaTool.saveSosenba45(buffer, 2222, $U.Seibetsu.Boba);
-    const cccc = await this.kolUmaTool.saveSosenba45(buffer, 1422, $U.Seibetsu.Boba, ccccc.Id, cccch.Id);
+    const cccch = await this.saveSosenba45(buffer, 2272, $U.Seibetsu.Hinba);
+    const ccccc = await this.saveSosenba45(buffer, 2222, $U.Seibetsu.Boba);
+    const cccc = await this.saveSosenba45(buffer, 1422, $U.Seibetsu.Boba, ccccc.Id, cccch.Id);
 
-    return this.kolUmaTool.saveSosenba123(buffer, 638, $U.Seibetsu.Hinba, cccc.Id, ccch.Id);
+    return this.saveSosenba123(buffer, 638, $U.Seibetsu.Hinba, cccc.Id, ccch.Id);
   }
 }
--- src/app/reader/KOL/KD3/KolKetKd3.ts
+++ src/app/reader/KOL/KD3/KolKetKd3.ts
@@ -6,7 +6,6 @@ import { UmaDao } from "../../../daos/UmaDao";
 import { Uma } from "../../../entities/Uma";
 import { DataToImport } from "../../DataToImport";
 import { Tool } from "../../Tool";
-import { KolUmaTool } from "../KolUmaTool";
 import {
   readInt,
   readStr,
@@ -24,32 +23,75 @@ export class KolKetKd3 extends DataToImport {
   @Inject()
   private umaDao: UmaDao;
 
-  @Inject()
-  private kolUmaTool: KolUmaTool;
-
   protected getBufferLength() {
     return 2010;
   }
 
+  protected async saveSosenba1(buffer: Buffer, offset: number, seibetsu: $U.Seibetsu, chichiUmaId?: number, hahaUmaId?: number) {
+    const bamei = readStr(buffer, offset + 8, 34);
+    if (!bamei) {
+      return null;
+    }
+
+    const uma = new Uma();
+    uma.Seibetsu = seibetsu;
+    uma.YunyuubaFlag = $U.yunyuubaFlag.toCodeFromKol(buffer, offset, 1);
+    uma.KolUmaCode = readInt(buffer, offset + 1, 7);
+    if (this.tool.isEnglish(bamei)) {
+      uma.EigoBamei = bamei;
+    } else {
+      uma.KanaBamei = bamei;
+    }
+    uma.EigoBamei = readStr(buffer, offset + 42, 34);
+    uma.Seinen = readInt(buffer, offset + 76, 4);
+    uma.Keiro = $U.keiro.toCodeFromKol(buffer, offset + 80, 2);
+    uma.Kesshu = $U.keiro.toCodeFromKol(buffer, offset + 82, 2);
+    uma.ChichiUmaId = chichiUmaId;
+    uma.HahaUmaId = hahaUmaId;
+
+    return this.umaDao.saveUma(uma, true);
+  }
+
+  protected async saveSosenba23(buffer: Buffer, offset: number, seibetsu: $U.Seibetsu, chichiUmaId?: number, hahaUmaId?: number) {
+    const bamei = readStr(buffer, offset + 8, 34);
+    if (!bamei) {
+      return null;
+    }
+
+    const uma = new Uma();
+    uma.Seibetsu = seibetsu;
+    uma.YunyuubaFlag = $U.yunyuubaFlag.toCodeFromKol(buffer, offset, 1);
+    uma.KolUmaCode = readInt(buffer, offset + 1, 7);
+    if (this.tool.isEnglish(bamei)) {
+      uma.EigoBamei = bamei;
+    } else {
+      uma.KanaBamei = bamei;
+    }
+    uma.ChichiUmaId = chichiUmaId;
+    uma.HahaUmaId = hahaUmaId;
+
+    return this.umaDao.saveUma(uma, true);
+  }
+
   public async save(buffer: Buffer) {
-    const hhh = await this.kolUmaTool.saveSosenba23(buffer, 667, $U.Seibetsu.Hinba);
-    const hhc = await this.kolUmaTool.saveSosenba23(buffer, 625, $U.Seibetsu.Boba);
-    const hh = await this.kolUmaTool.saveSosenba23(buffer, 332, $U.Seibetsu.Hinba, hhc.Id, hhh.Id);
+    const hhh = await this.saveSosenba23(buffer, 667, $U.Seibetsu.Hinba);
+    const hhc = await this.saveSosenba23(buffer, 625, $U.Seibetsu.Boba);
+    const hh = await this.saveSosenba23(buffer, 332, $U.Seibetsu.Hinba, hhc.Id, hhh.Id);
 
-    const hch = await this.kolUmaTool.saveSosenba23(buffer, 583, $U.Seibetsu.Hinba);
-    const hcc = await this.kolUmaTool.saveSosenba23(buffer, 542, $U.Seibetsu.Boba);
-    const hc = await this.kolUmaTool.saveSosenba23(buffer, 289, $U.Seibetsu.Boba, hcc.Id, hch.Id);
+    const hch = await this.saveSosenba23(buffer, 583, $U.Seibetsu.Hinba);
+    const hcc = await this.saveSosenba23(buffer, 542, $U.Seibetsu.Boba);
+    const hc = await this.saveSosenba23(buffer, 289, $U.Seibetsu.Boba, hcc.Id, hch.Id);
 
-    const chh = await this.kolUmaTool.saveSosenba23(buffer, 499, $U.Seibetsu.Hinba);
-    const chc = await this.kolUmaTool.saveSosenba23(buffer, 457, $U.Seibetsu.Boba);
-    const ch = await this.kolUmaTool.saveSosenba23(buffer, 247, $U.Seibetsu.Hinba, chc.Id, chh.Id);
+    const chh = await this.saveSosenba23(buffer, 499, $U.Seibetsu.Hinba);
+    const chc = await this.saveSosenba23(buffer, 457, $U.Seibetsu.Boba);
+    const ch = await this.saveSosenba23(buffer, 247, $U.Seibetsu.Hinba, chc.Id, chh.Id);
 
-    const cch = await this.kolUmaTool.saveSosenba23(buffer, 415, $U.Seibetsu.Hinba);
-    const ccc = await this.kolUmaTool.saveSosenba23(buffer, 373, $U.Seibetsu.Boba);
-    const cc = await this.kolUmaTool.saveSosenba23(buffer, 205, $U.Seibetsu.Hinba, ccc.Id, cch.Id);
+    const cch = await this.saveSosenba23(buffer, 415, $U.Seibetsu.Hinba);
+    const ccc = await this.saveSosenba23(buffer, 373, $U.Seibetsu.Boba);
+    const cc = await this.saveSosenba23(buffer, 205, $U.Seibetsu.Hinba, ccc.Id, cch.Id);
 
-    const h = await this.kolUmaTool.saveSosenba1(buffer, 121, $U.Seibetsu.Hinba, hc.Id, hh.Id);
-    const c = await this.kolUmaTool.saveSosenba1(buffer, 37, $U.Seibetsu.Boba, cc.Id, ch.Id);
+    const h = await this.saveSosenba1(buffer, 121, $U.Seibetsu.Hinba, hc.Id, hh.Id);
+    const c = await this.saveSosenba1(buffer, 37, $U.Seibetsu.Boba, cc.Id, ch.Id);
 
     const uma = new Uma();
     uma.KolUmaCode = readInt(buffer, 0, 7);
